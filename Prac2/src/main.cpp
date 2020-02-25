ae6450f2db276f1c9eeb67d02722b4fd3f65ff8c
Shaun Naude <shaunnaude20@gmail.com>
2020-02-25 20:54:00 +0200

    server is working

--- Prac2/src/main.cpp
+++ Prac2/src/main.cpp
@@ -35,6 +35,69 @@ int main() {
     
     bind(Listening, (sockaddr*)&mySocketInfo, sizeof(mySocketInfo));
 
+    //start listening , SOMAXCONN=127 connections
+    listen(Listening, SOMAXCONN);
 
+
+    //setting up the client / accept call
+
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+ 
+    int clientSocket = accept(Listening, (sockaddr*)&client, &clientSize);
+ 
+    char host[NI_MAXHOST];      // Client's remote name
+    char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
+
+     memset(host, 0, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+     memset(service, 0, NI_MAXSERV);
+
+     if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+    {
+        cout << host << " connected on port " << service << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+    }
+ 
+    // Close listening socket
+    close(Listening);
+ 
+    // While loop: accept and echo message back to client
+    char buf[4096];
+ 
+    while (true)
+    {
+        memset(buf, 0, 4096);
+ 
+        // Wait for client to send data
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            cerr << "Error in recv(). Quitting" << endl;
+            break;
+        }
+ 
+        if (bytesReceived == 0)
+        {
+            cout << "Client disconnected " << endl;
+            break;
+        }
+ 
+        cout << string(buf, 0, bytesReceived) << endl;
+ 
+        // Echo message back to client
+        send(clientSocket, buf, bytesReceived + 1, 0);
+    }
+ 
+    // Close the socket
+    close(clientSocket);
+ 
+    return 0;
+
+ 
 
 }
